0f5f3a9074460802b3bab361dae7c9166a788346
Martin Schoeberl <martin@jopdesign.com>
2022-05-03 23:07:57 +0200

    memory

--- src/main/scala/Memory.scala
+++ src/main/scala/Memory.scala
@@ -1,8 +1,5 @@
 import chisel3._
 
-
-
-//- start memory_forwarding
 class Memory() extends Module {
   val io = IO(new Bundle {
     val rdAddr = Input(UInt(10.W))
@@ -25,9 +22,8 @@ class Memory() extends Module {
 
   io.rdData := Mux(doForwardReg, wrDataReg, memData)
 }
-//- end
 
 object ForwardingMemory extends App {
   emitVerilog(new Memory(), Array("--target-dir", "generated"))
-  // emitVerilog(new TrueDualPortMemory(), Array("--target-dir", "generated", "--target:fpga"))
+  // emitVerilog(new Memory(), Array("--target-dir", "generated", "--target:fpga"))
 }
